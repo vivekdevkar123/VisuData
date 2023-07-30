5a0b7550074dfe8db59ca4a884ce827500c7cef4
vivek devkar <mrvivekdevkar123@gmail.com>
2023-07-30 19:46:46 +0530

    file upload and delete from local storage

--- VisuData/settings.py
+++ VisuData/settings.py
@@ -131,6 +131,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 
 MEDIA_URL = '/media/'
 
