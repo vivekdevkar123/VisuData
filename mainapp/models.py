fe3bcd4de752a6523b77fa4a756420e03bde1100
vivek devkar <mrvivekdevkar123@gmail.com>
2023-08-08 00:45:38 +0530

    Contact us page created

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ContactU(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=13)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import ContactU
+from django.contrib import messages
+
 # Create your views here.
 
 def home(request):
     return render(request,'basic/index.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        description = request.POST.get('desc')
+
+        contactus = ContactU(name=name,email=email,phone=phone,description=description)
+        contactus.save()
+        messages.success(request, "Thank You For Your Feedback!!!")
+
+        return redirect('home')
+    
     return render(request,'basic/contact.html')
 
 def about(request):
