96e4a6634108d2a53ae1b03dfe1e68249aa75f3a
vivek devkar <mrvivekdevkar123@gmail.com>
2023-08-17 11:30:52 +0530

    for branch change

---
+++ Visulization/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('<slug:id>/',Visulization_Home,name='Visulization-Home'),
+    path('scatter/<slug:id>', scatter_plot, name='scatter_plot'),
+    path('line/<slug:id>', line_plot, name='line_plot'),
+    path('bar/<slug:id>', bar_chart, name='bar_chart'),
+    path('histogram/<slug:id>', histogram, name='histogram'),
+    path('save_plot/<int:id>', Save_Plot, name='Save-Plot'),
+    path('delete_plot/<int:id>', Delete_Plot, name='Delete-Plot'),
+    path('delete_plot_img/<int:id>', Delete_Plot_Img, name='Delete-Plot-img'),
+]
---
+++ Visulization/views.py
@@ -0,0 +1,222 @@
+from django.shortcuts import render,redirect
+import pandas as pd
+from EDA.models import Dataset
+import json
+import matplotlib.pyplot as plt
+from io import BytesIO, StringIO
+import base64
+from .models import Plot
+from django.contrib import messages
+import seaborn as sns
+
+
+def Visulization_Home(request, id):
+    # Check if a plot image is stored in the session
+    plot_image = request.session.get('plot_image', None)
+    
+    if plot_image:
+        context = {'plot_image': plot_image}
+    else:
+        context = {}  # Initialize an empty context if no plot image
+    
+    dataset = Dataset.objects.get(dataset_id=id)
+    df = pd.read_csv(dataset.uploaded_file.path)
+    columns = df.select_dtypes(include=['number'])
+    column_names = columns.columns.tolist()[1:]
+
+    context.update({
+        'title': dataset.dataset_name,
+        'my_id': dataset.dataset_id,
+        'column_names': column_names,
+    })
+
+    return render(request, 'visulization/index.html', context)
+
+
+def scatter_plot(request, id):
+    dataset = Dataset.objects.get(dataset_id=id)
+    df = pd.read_csv(dataset.uploaded_file.path)
+    column_names = df.columns.tolist()
+    
+    if request.method == 'POST':
+        heading = request.POST.get('scatter-plot-heading')
+        x_axis_label = request.POST.get('x-axis-label')
+        y_axis_label = request.POST.get('y-axis-label')
+        x_axis_column = request.POST.get('x_axis')
+        y_axis_column = request.POST.get('y_axis')
+        
+        plt.figure(figsize=(10, 6))
+        plt.scatter(df[x_axis_column], df[y_axis_column])
+        plt.title(heading)
+        plt.xlabel(x_axis_label)
+        plt.ylabel(y_axis_label)
+        
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        
+        plot_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        
+        # Save the plot_image to the session to pass it to the home view
+        request.session['plot_image'] = plot_image
+        
+        return redirect('Visulization-Home',id=id)
+    
+    context = {
+        'title': dataset.dataset_name,
+        'my_id': dataset.dataset_id,
+        'column_names': column_names,
+    }
+    return render(request, 'visulization/index.html', context)
+
+
+def line_plot(request, id):
+    dataset = Dataset.objects.get(dataset_id=id)
+    df = pd.read_csv(dataset.uploaded_file.path)
+    column_names = df.columns.tolist()
+    
+    if request.method == 'POST':
+        heading = request.POST.get('line-plot-heading')
+        y_axis_label = request.POST.get('y-axis-label')
+        y_axis_column = request.POST.get('y_axis')
+        line_color = request.POST.get('line_color')
+        
+        plt.figure(figsize=(10, 6))
+        
+        # Use Seaborn's line plot
+        sns.lineplot(data=df, x=df.index, y=y_axis_column, color=line_color)
+        
+        plt.title(heading)
+        plt.ylabel(y_axis_label)
+        
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        
+        plot_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        
+        # Save the plot_image to the session to pass it to the home view
+        request.session['plot_image'] = plot_image
+        
+        return redirect('Visulization-Home', id=id)
+    
+    context = {
+        'title': dataset.dataset_name,
+        'my_id': dataset.dataset_id,
+        'column_names': column_names,
+    }
+    return render(request, 'visulization/index.html', context)
+
+
+
+
+
+
+def bar_chart(request, id):
+    dataset = Dataset.objects.get(dataset_id=id)
+    df = pd.read_csv(dataset.uploaded_file.path)
+    column_names = df.columns.tolist()
+    
+    if request.method == 'POST':
+        heading = request.POST.get('bar-chart-heading')
+        x_axis_label = request.POST.get('x-axis-label')
+        y_axis_label = request.POST.get('y-axis-label')
+        x_axis_column = request.POST.get('x_axis')
+        y_axis_column = request.POST.get('y_axis')
+        bar_color = request.POST.get('bar_color')
+        
+        plt.figure(figsize=(10, 6))
+        
+        # Create a bar chart using Matplotlib
+        plt.bar(df[x_axis_column], df[y_axis_column], color=bar_color)
+        plt.title(heading)
+        plt.xlabel(x_axis_label)
+        plt.ylabel(y_axis_label)
+        plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+        
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        
+        plot_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        
+        # Save the plot_image to the session to pass it to the home view
+        request.session['plot_image'] = plot_image
+        
+        return redirect('Visulization-Home', id=id)
+    
+    context = {
+        'title': dataset.dataset_name,
+        'my_id': dataset.dataset_id,
+        'column_names': column_names,
+    }
+    return render(request, 'visulization/index.html', context)
+
+
+
+
+def histogram(request, id):
+    dataset = Dataset.objects.get(dataset_id=id)
+    df = pd.read_csv(dataset.uploaded_file.path)
+    column_names = df.columns.tolist()
+    
+    if request.method == 'POST':
+        heading = request.POST.get('histogram-heading')
+        x_axis_label = request.POST.get('x-axis-label')
+        x_axis_column = request.POST.get('x_axis')
+        histogram_color = request.POST.get('histogram_color')
+        
+        plt.figure(figsize=(10, 6))
+        
+        # Create a histogram using Matplotlib
+        plt.hist(df[x_axis_column], bins='auto', color=histogram_color)
+        plt.title(heading)
+        plt.xlabel(x_axis_label)
+        plt.ylabel('Frequency')
+        
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        
+        plot_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        
+        # Save the plot_image to the session to pass it to the home view
+        request.session['plot_image'] = plot_image
+        
+        return redirect('Visulization-Home', id=id)
+    
+    context = {
+        'title': dataset.dataset_name,
+        'my_id': dataset.dataset_id,
+        'column_names': column_names,
+    }
+    return render(request, 'visulization/index.html', context)
+
+
+
+
+
+def Save_Plot(request, id):
+    plot_image = request.session.get('plot_image', None)
+    dataset = Dataset.objects.get(dataset_id=id)
+    if plot_image:
+        plot = Plot(dataset=dataset, data={'plot_image': plot_image})
+        plot.save()
+        messages.success(request, "Plot saved successfully!!!")
+        del request.session['plot_image']
+        
+    return redirect('Visulization-Home', id=id)
+
+
+def Delete_Plot(request, id):
+    del request.session['plot_image']
+    messages.warning(request, "Plot deleted successfully!!!")
+    return redirect('Visulization-Home', id=id)
+
+
+def Delete_Plot_Img(request, id):
+    img = Plot.objects.get(plot_id = id)
+    img.delete()
+    messages.warning(request, "Plot deleted successfully!!!")
+    id = img.dataset.dataset_id
+    return redirect('data-analysis', id=id)
