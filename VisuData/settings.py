90d85e6a445d7c1dc26535ebd809fe56a8dfb4cc
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2024-07-14 23:24:13 +0530

    static files added

--- VisuData/settings.py
+++ VisuData/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -57,8 +57,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'VisuData.urls'
-
-import os
 
 TEMPLATES = [
     {
@@ -128,10 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # Path where the static files will be collected
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
