db75867418fd66456bc5a5e2eff18b99b1d9db35
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2024-07-15 12:37:00 +0530

    media query added

--- VisuData/urls.py
+++ VisuData/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('myadminpannel/', admin.site.urls),
     path('',include('mainapp.urls')),
     path("users/", include("users.urls")),
     path('data/',include('EDA.urls')),
